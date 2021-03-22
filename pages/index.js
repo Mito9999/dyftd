b71f73ca4a9b0a38bdc0b0012285d8863428e6c2
Mito6211 <willmoore674@gmail.com>
2021-03-22 12:26:55 -0400

    Add meta and title tags

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,39 @@
+import Head from "next/head";
 import { useState, useEffect } from "react";
-import CreateReactAppEntryPoint from "@components/App";
+import AppComponent from "@components/App";
 
-function App() {
+export default function App() {
   const [isMounted, setIsMounted] = useState(false);
-
   useEffect(() => {
     setIsMounted(true);
   }, []);
 
-  if (!isMounted) {
-    return null;
-  }
+  return (
+    <>
+      <Head>
+        <title>DYFTD - Did you feed the dog?</title>
+        <meta
+          name="description"
+          content="Easy to use collaborative toggles suitable to any purpose"
+        />
+        <meta property="og:type" content="app" />
+        <meta property="og:title" content="DYFTD" />
+        <meta name="twitter:title" content="DYFTD" />
+        <meta
+          property="og:description"
+          content="Collaborate through simple toggles"
+        />
+        <meta
+          name="twitter:description"
+          content="Collaborate through simple toggles"
+        />
+        <meta property="og:image" content="../public/example.png" />
+        <meta name="twitter:image" content="../public/example.png" />
 
-  return <CreateReactAppEntryPoint />;
+        <meta property="og:url" content="https://dyftd.vercel.app/" />
+        <meta property="og:site_name" content="DYFTD" />
+      </Head>
+      {isMounted && <AppComponent />}
+    </>
+  );
 }
-
-export default App;
